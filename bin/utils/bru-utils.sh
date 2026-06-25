3f6085bc69f2323f439116c793a94374a9f945d5
Richard Hess <rhess@shield-legal.com>
2026-06-25 08:23:50 -0700

    fixes

---
+++ bin/utils/bru-utils.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# ---------------------------------------------------------------------
+# ** for whatever reason, bru is continually complaining about these...
+
+bru::trust() {
+  local FORMULA=(
+    f/mcptools/mcp
+    ksdme/tap/ut
+    mistertea/et/et
+    rjyo/moshi/moshi-hook
+    rshelekhov/tap/lazymake
+  )
+  local TAPS=(
+    charmbracelet/tap
+    github/gh
+    wedow/tools
+  )
+  for formula in "${FORMULA[@]}"
+  do
+    brew trust --formula ${formula}
+  done
+  for tap in "${TAPS[@]}"
+  do
+    brew trust ${tap}
+  done
+}
